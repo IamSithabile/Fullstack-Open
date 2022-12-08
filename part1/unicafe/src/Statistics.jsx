8d11413576c52d0c1df72360256290e2ea4d26a0
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-08 19:13:58 +0200

    Exercise 1.7

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Statistics = ({ good, bad, neutral }) => {
+  let all = good + bad + neutral;
   return (
     <>
       <h1>Statistics</h1>
       <p> Good : {good}</p>
       <p> Neutral : {neutral}</p>
       <p> Bad : {bad}</p>
+      <p>All: {all} </p>
+      <p>Average: {(good - bad) / all} </p>
+      <p>Positive: {(good / all) * 100} %</p>
     </>
   );
 };
