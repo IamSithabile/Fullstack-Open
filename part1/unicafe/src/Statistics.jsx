3d3c2564def02c70809b482c287699e15996fef7
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-08 19:17:44 +0200

    Exercise 1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -2,6 +2,15 @@ import React from "react";
 
 const Statistics = ({ good, bad, neutral }) => {
   let all = good + bad + neutral;
+
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <div>No feedback given</div>
+      </>
+    );
+  }
   return (
     <>
       <h1>Statistics</h1>
