eb98804388ad85a3393f62eeee23e86bf5932749
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-09 19:49:42 +0200

    Exercise 2.6 Phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const nameHandler = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const personObj = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObj));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={formSubmitHandler}>
+        <div>
+          name: <input value={newName} onChange={nameHandler} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
