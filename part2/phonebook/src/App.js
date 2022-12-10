e77bb4ddf02672641ba34fa8270b994ff956980f
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-10 09:23:28 +0200

    Exercise 2.9 Phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [query, setQuery] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -29,9 +35,20 @@ const App = () => {
     setNewName("");
   };
 
+  const filterListHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filteredList = persons.filter((person) =>
+    person.name.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter the list : <input value={query} onChange={filterListHandler} />
+      </div>
       <form onSubmit={formSubmitHandler}>
         <div>
           Name: <input value={newName} onChange={nameHandler} />
@@ -44,7 +61,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredList.map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
           </li>
